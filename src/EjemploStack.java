c8cac193292c9e9ad95c769d454ed806b6e0244a
GukCM <gusavocod@gmail.com>
2023-06-28 02:46:18 -0600

    StackScanner and New Main

---
+++ src/EjemploStack.java
@@ -0,0 +1,8 @@
+
+import java.util.Stack;
+
+    public class EjemploStack {
+        public static void main(String[] args) {
+
+        }
+    }
---
+++ src/com/amigoscode/StackScannerJava.java
@@ -0,0 +1,78 @@
+package com.amigoscode;
+
+public class StackScannerJava {
+
+    /*
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your Name?");
+        String name = scanner.nextLine();
+        System.out.println("What is your Age?");
+        Integer age = scanner.nextInt();
+        System.out.println("Hello, " + name + "!");
+        System.out.println("You have: " + age + " years");
+}
+
+     */
+    public static void main(String[] args) {
+        /*Stack<String> pila = new Stack<String>();
+
+        // Agregar elementos a la pila
+        pila.push("Elemento 1");
+        pila.push("Elemento 2");
+        pila.push("Elemento 3");
+        pila.push("Elemento 5");
+        pila.push("Elemento 6");
+        pila.push("Elemento 7");
+        pila.push("Elemento 8");
+
+        // Obtener el elemento en la parte superior de la pila sin eliminarlo
+        String elementoSuperior = pila.peek();
+        System.out.println("Elemento superior: " + elementoSuperior);
+
+        // Eliminar el elemento en la parte superior de la pila
+        String elementoEliminado = pila.pop();
+        System.out.println("Elemento eliminado: " + elementoEliminado);
+
+        // Verificar si la pila está vacía
+        boolean estaVacia = pila.isEmpty();
+        System.out.println("¿La pila está vacía? " + estaVacia);
+
+        // Obtener el tamaño de la pila
+        int tamanoPila = pila.size();
+        System.out.println("Tamaño de la pila: " + tamanoPila);*/
+        /*
+        //Creacon de Arraylist
+        List<String> ListaColores = new ArrayList<>();
+        //Creacion mediante la clase Arrays
+        String [] nombres = {"Arturo", "Daniel", "Edgar"};
+        List<String> listaNombres = Arrays.asList(nombres);
+        System.out.println(listaNombres);
+        //Adicion de elementos individuales
+        ListaColores.add("Azul");
+        ListaColores.add("Amarillo");
+        ListaColores.add("Morado");
+        System.out.println(ListaColores);
+        System.out.println(ListaColores.size());
+        //Crear lista a partir de otra lista
+        List<String> elementos = new ArrayList<>(ListaColores);
+        System.out.println(elementos);
+        //Combinar 2 listas
+        elementos.addAll(listaNombres);
+        System.out.println("Lista elementos");
+        System.out.println(elementos);
+        System.out.println("Lista nombres");
+        listaNombres.forEach(System.out::println);
+        ListaColores.forEach(System.out::println);
+        ListIterator<String> iterador = ListaColores.listIterator();
+        System.out.println("Recorrido con iterador");
+        while(iterador.hasNext()){
+            System.out.println("Color: " + iterador.next());
+        }        System.out.println("Recorrido con iterador");
+        while(iterador.hasPrevious()){
+            System.out.println("Color: " + iterador.previous());
+        }
+*/
+    }
+}
